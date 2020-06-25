d72bfd3d2d9114a7ccd179a7627cc734009be3bb
sweyome <sweyome1@gmail.com>
2020-06-24 21:43:51 -0400

    Back-end model and routerupdate

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const contactRouter = require('./Api/router');
 
 
 const server = express();
 server.use(express.json);
+server.use('/api/contacts', contactRouter);
 
 server.get('/', (req, res) =>{
     res.status(200).json({message: 'server is running'})
