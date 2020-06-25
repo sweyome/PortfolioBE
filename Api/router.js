a358b736ebe2f201bc8648703dc765ac58a7086d
sweyome <sweyome1@gmail.com>
2020-06-24 21:39:49 -0400

    Back-end model and router

---
+++ Api/router.js
@@ -0,0 +1,37 @@
+const db = require('../Database/model/model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  db.find()
+  .then(contact => {
+    res.status(200).json(contact);
+  })
+  .catch (err => {
+    res.status(500).json({ message: 'Failed to get users' });
+  });
+});
+
+router.post('/', (req, res) => {
+  const userData = req.body;
+//! resolve to newly create users
+  db.add(userData)
+});
+//! remove 
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+ db.remove(id)
+  .then(count => {
+    if (count) {
+      res.json({ removed: count });
+    } else {
+      res.status(404).json({ message: 'Could not find user with given id' });
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to delete user' });
+  });
+});
+
+module.exports = router;
---
+++ Database/model/model.js
@@ -0,0 +1,19 @@
+const db = require('../dbconfig');
+
+module.exports = {
+    find,
+    add,
+    remove
+};
+
+function find(){
+    return db('contact')
+}
+
+function add(user){
+return db('contact').insert(userData)
+}
+// resolve to a count
+function remove(id){
+return db('contact').where({ id }).del()
+}
