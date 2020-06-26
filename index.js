2416e6c1085b1461f7914f81f8d8c99c591f4f95
sweyome <sweyome1@gmail.com>
2020-06-25 20:58:49 -0400

    git ignore2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const server = require('./server.js');
-
+const port = 5000
 
 server.listen(5000, () =>{
-    console.log('server listening on local host')
+    console.log(`server listening on local host port ${port}`)
 })
 
 
