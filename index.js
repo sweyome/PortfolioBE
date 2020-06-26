5286414a11b29674007e97dacd44858f547ef0e3
sweyome <sweyome1@gmail.com>
2020-06-25 20:54:03 -0400

    git ignore

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require('./server');
+const server = require('./server.js');
 
 
 server.listen(5000, () =>{
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const contactRouter = require('./Api/router');
 
 
 const server = express();
 server.use(express.json);
+server.use(cors())
 server.use('/api/contacts', contactRouter);
 
 server.get('/', (req, res) =>{
